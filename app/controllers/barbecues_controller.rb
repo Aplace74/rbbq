daf9e013e8890b9c67e26a9d92a1e613ae6f3c6a
Aymeric DHALLUIN <aymeric.dhalluin@gmail.com>
2019-05-27 17:12:03 +0200

    add new create BBQController

--- app/controllers/barbecues_controller.rb
+++ app/controllers/barbecues_controller.rb
@@ -1,2 +1,19 @@
 class BarbecuesController < ApplicationController
+	def new
+		@user = User.find(params[:user_id])
+		@barbecue = Barbecue.new
+	end
+
+	def create
+		@barbecue = Barbecue.new
+		@barbecue.user = User.find(params[:user_id])
+		@barbecue.save
+	end
+
+	private
+
+	def barbecue_params
+		params.require(:barbecue).permit(:content)
+	end
+	
 end
