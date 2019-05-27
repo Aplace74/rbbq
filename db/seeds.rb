cdbadeb54ddaf2debcdef58d6649d08368043c11
Christophe Lepretre <contact@start-me-up.fr>
2019-05-27 18:52:07 +0200

    qques bbq ajoutés

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,8 +46,6 @@ users = [chris, aym, anto, max]
 
 puts "Users created, creating Barbecues ..."
 
-4.times do 
-
 bbq1 = Barbecue.create!(
     name: "Barbecue Charbon Mendy-Alde Baïa Le Marquier",
     model: "Le Marquier",
@@ -55,7 +53,7 @@ bbq1 = Barbecue.create!(
     description: "Ce barbecue robuste est encastré dans un chariot pour pouvoir le déplacer facilement. Le chariot est équipé de 2 larges roues pour faciliter le déplacement sur tout type de terrain.",
     price: rand(30..40),
     free: true,
-    capacity: "12+",
+    capacity: "12",
     types: "coal",
     rating: [0, 1, 2, 3, 4, 5].sample,
     user: users.sample
@@ -63,6 +61,36 @@ bbq1 = Barbecue.create!(
 bbq1.remote_pictures_url = "https://www.esprit-barbecue.fr/5446-thickbox_default/barbecue-charbon-mendy-alde-le-marquier.jpg"
 bbq1.save
 
-end
+bbq2 = Barbecue.create!(
+    name: "Barbecue charbon Weber Compact Kettle 47 cm",
+    model: "Weber",
+    address: Faker::Address.full_address,
+    description: "Barbecue léger, de petite taille, facilement mobile. L’accroche couvercle vous évite de le reposer dans votre pelouse et de la brûler.",
+    price: rand(20..30),
+    free: true,
+    capacity: "8",
+    types: "coal",
+    rating: [0, 1, 2, 3, 4, 5].sample,
+    user: users.sample
+)
+bbq2.remote_pictures_url = "https://www.esprit-barbecue.fr/96-thickbox_default/barbecue-weber-compact-kettle-47-cm.jpg"
+bbq2.save
+
+bbq3 = Barbecue.create!(
+    name: "Barbecue Weber gaz Q3200",
+    model: "Weber",
+    address: Faker::Address.full_address,
+    description: "L'espace de cuisson est large, idéal permettant de cuisiner pour un grand nombre de personnes. Equipé de deux brûleurs, vous pouvez réaliser deux types de cuisson: directe ou indirecte.",
+    price: rand(30..40),
+    free: true,
+    capacity: "12",
+    types: "gas",
+    rating: [0, 1, 2, 3, 4, 5].sample,
+    user: users.sample
+)
+bbq3.remote_pictures_url = "https://www.esprit-barbecue.fr/1751-thickbox_default/barbecue-weber-gaz-q3200.jpg"
+bbq3.save
+
+
 
 puts "seed finished !"
