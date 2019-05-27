f5ba1731bb395a08c2035dc986834d2d006956c3
Christophe Lepretre <contact@start-me-up.fr>
2019-05-27 16:36:53 +0200

    debut seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "clearing databases ..."
+Barbecue.detroy_all
+User.destroy_all
+
+puts "Cleaning OK, creating users ..."
+
+10.times do
+    
+end
+
+puts "Users created, creating Barbecues ..."
+
+puts "seed finished !"
