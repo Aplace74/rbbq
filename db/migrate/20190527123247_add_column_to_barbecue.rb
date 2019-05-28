348bfd5691cfad5ed435bf1b1ef304195257c75e
Antoine Place <48134202+Aplace74@users.noreply.github.com>
2019-05-28 13:50:23 +0200

    Update 20190527123247_add_column_to_barbecue.rb

--- db/migrate/20190527123247_add_column_to_barbecue.rb
+++ db/migrate/20190527123247_add_column_to_barbecue.rb
@@ -1,6 +1,6 @@
 class AddColumnToBarbecue < ActiveRecord::Migration[5.2]
   def change
     add_column :barbecues, :capacity, :integer
-    add_column :barbecues, :type, :string
+    add_column :barbecues, :types, :string
   end
 end
