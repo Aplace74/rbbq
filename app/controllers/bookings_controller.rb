bd71b8b0c32172110ca15e105a947f9d03ff7407
Christophe Lepretre <contact@start-me-up.fr>
2019-05-28 11:13:45 +0200

    need to update master - controller not finished

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,35 @@
 class BookingsController < ApplicationController
+
+    def new
+        @booking = Booking.new
+    end
+
+    def create
+        @booking = Booking.new(booking_params)
+        @booking.save
+        redirect_to dashboard_users_path(@user)
+    end
+
+    def edit
+        @booking = Booking.find(params[:id])
+    end
+
+    def update
+        @booking = Booking.find(params[:id])
+        @booking.update(booking_params)
+        redirect_to dashboard_users_path(@user)
+    end
+
+    def destroy
+        @booking = Booking.find(params[:id])
+        @booking.destroy
+        redirect_to dashboard_users_path(@user)
+    end
+
+    private
+
+    def booking_params
+        params.require(:booking).permit(:start_date, :end_date)
+    end
+
 end
