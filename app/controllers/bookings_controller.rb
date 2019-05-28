f2c216cd98e4b8d85394bcb666ae284fd982ad91
Christophe Lepretre <contact@start-me-up.fr>
2019-05-28 12:15:00 +0200

    bookings controller new, create, edit, update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,24 @@
 class BookingsController < ApplicationController
 
     def new
+        @barbecue = Barbecue.find(params[:barbecue_id])
         @booking = Booking.new
     end
 
     def create
+        @barbecue = Barbecue.find(params[:barbecue_id])
         @booking = Booking.new(booking_params)
-        @booking.save
-        redirect_to dashboard_users_path(@user)
+        @booking.barbecue = @barbecue
+        @booking.user = current_user
+        if @booking.save
+            redirect_to dashboard_users_path(@user)
+          else
+            render :new
+          end      
     end
 
     def edit
+        @barbecue = Barbecue.find(params[:barbecue_id])
         @booking = Booking.find(params[:id])
     end
 
