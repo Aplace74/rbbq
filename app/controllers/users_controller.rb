839d24ec5ef9d07d661f535f48c198db9a70e260
Maxime Derouet <mxderouet@gmail.com>
2019-05-29 14:08:43 +0200

    pundit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
+  
   def dashboard
     @user = current_user
+    authorize @user
     @bookings = Booking.where(@user.id == :user_id)
     @title = "#{@user.first_name} #{@user.last_name} Dashboard"
   end
 
   def show
     @user = User.find(params[:id])
+    authorize @user
     @title = "#{@user.first_name} #{@user.last_name}"
   end
 end
