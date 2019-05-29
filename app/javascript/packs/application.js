b5cda8e89218da9a9747374795306f3d7a3f2f55
Aymeric DHALLUIN <aymeric.dhalluin@gmail.com>
2019-05-29 12:21:24 +0200

    Confilict resolve

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,9 @@
 import "bootstrap";
-<<<<<<< HEAD
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
-
-initMapbox();
-=======
 import { calculatePrice } from '../plugins/calculate-price';
 
+initMapbox();
 calculatePrice()
->>>>>>> master
+
