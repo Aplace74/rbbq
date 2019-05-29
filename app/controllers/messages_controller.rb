85cbb88f085ae667f4b92227b9aac802ca2fb013
Antoine Place <antoineplace2212@gmail.com>
2019-05-29 17:03:59 +0200

    save

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,26 @@
 class MessagesController < ApplicationController
   def index
-    @sent = Message.where("sender= ?", current_user).order(:created_at)
-    @receipt = Message.where("receiver= ?", current_user).order(:created_at)
+    @sent = Message.where(sender_id: current_user.id).order(:created_at)
+    @receipt = Message.where(receiver_id: current_user.id).order(:created_at)
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.sender_id = current_user.id
+    if @message.save
+      redirect_to messages_path(current_user)
+    else
+      render :index
+    end
   end
 
   def chat
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:receiver_id, :content)
   end
 end
