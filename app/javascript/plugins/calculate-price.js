92bf3ec7d988a0bc268ca1d3794d34c1c39fa4ca
Christophe Lepretre <contact@start-me-up.fr>
2019-05-29 14:14:28 +0200

    booking JS cleaned and improved with alerts

--- app/javascript/plugins/calculate-price.js
+++ app/javascript/plugins/calculate-price.js
@@ -1,4 +1,3 @@
-console.log("hello")
 const calculatePrice = () => {
     const form = document.querySelector("#new_booking");
     if (form) {
@@ -7,40 +6,34 @@ const calculatePrice = () => {
             let startMonth = parseInt(document.querySelector("#booking_start_date_2i").value, 10);
             let startDay = document.querySelector("#booking_start_date_3i").value;
             let startDate = new Date(startYear, (startMonth - 1), startDay);
-            console.log(startDate)
 
             let endYear = document.querySelector("#booking_end_date_1i").value;
             let endMonth = parseInt(document.querySelector("#booking_end_date_2i").value, 10);
             let endDay = document.querySelector("#booking_end_date_3i").value;
             let endDate = new Date(endYear, (endMonth - 1), endDay);
-            console.log(endDate);
 
-            let oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-
+            const oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
             let diffDays = Math.round(Math.abs((startDate.getTime() - endDate.getTime())/(oneDay)));
             console.log(diffDays);
 
             const rentalPrice = parseInt(document.getElementById("booking_rentalprice").value, 10);
             const totalPrice = `${(diffDays + 1) * rentalPrice}€ for ${diffDays + 1} day(s) rental`;
-    
-            console.log(endDate < startDate)
+            const errorMessage1 = `<span class="text-danger">Start date can't be in the past</span>`;
+            const errorMessage2 = `<span class="text-danger">End date can't be before Start date</span>`;
+            const today = Date.now();
 
-            if (endDate < startDate) {
-                const errorMessage = `<span class="text-danger">End date can't be before Start date</span>`;
-                document.getElementById("price").innerHTML = errorMessage;
+             if (startDate < today) {
+                document.querySelector("input[type=submit]").disabled = true;
+                document.getElementById("price").innerHTML = errorMessage1;            
+             } else if (endDate < startDate) {
+                document.querySelector("input[type=submit]").disabled = true;
+                document.getElementById("price").innerHTML = errorMessage2;
             } else {
-                console.log("je suis false");
+                document.querySelector("input[type=submit]").disabled = false;
                 document.getElementById("price").innerHTML = totalPrice ;
-            };                
-
+            };
         });
     };
 };
 
 export { calculatePrice };
-
-// var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-// var firstDate = new Date(2008,01,12);
-// var secondDate = new Date(2008,01,22);
-
-// var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
