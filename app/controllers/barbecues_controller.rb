c7587bd6251f9a6660d76cf9951a6f3131c83a1e
Christophe Lepretre <contact@start-me-up.fr>
2019-05-31 13:29:00 +0200

    ajax reviews functional, not rating

--- app/controllers/barbecues_controller.rb
+++ app/controllers/barbecues_controller.rb
@@ -14,6 +14,7 @@ class BarbecuesController < ApplicationController
   
   def show
     @booking = Booking.new
+    @review = Review.new
     @marker = []
     @marker << 
       {
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,22 @@
 class ReviewsController < ApplicationController
-  def new
-    @barbecue = Barbecue.find(params[:barbecue_id])
-    @review = Review.new
-    authorize @review
-  end
 
   def create
     @barbecue = Barbecue.find(params[:barbecue_id])
     @review = Review.new(review_params)
-    authorize @review
     @review.barbecue = @barbecue
     @review.user = current_user
+    authorize @review
+
     if @review.save
-      redirect_to barbecue_path(@barbecue)
+      respond_to do |format|
+        format.html { redirect_to barbecue_path(@barbecue) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
     else
-      render :new
+      respond_to do |format|
+        format.html { render 'barbecues/show' }
+        format.js  # <-- idem
+      end
     end
   end
 
